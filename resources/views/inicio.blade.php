cb8de253d6a4203223f7e3123902ce926dacddfb
vanesa-dh <vanesap@gmx.com>
2019-07-29 14:47:31 -0300

    Agrego rutas - vistas - carpetas img y css

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,52 @@
+@extends("plantilla")
+
+@section ("principal")
+
+  <!-- INICIO del BANNER -->
+
+  {{-- Aca va un banner --}}
+  
+  <!-- FIN del BANNER -->
+
+  <!-- INICIO DE PRODUCTOS DE MUESTRA-->
+  <div class="products_container">
+
+    <div class="product_container">
+      <div class="product_imagen">
+          <a href="index.html"><img src="img/sweaterwhite.jpg" alt="photo"></a>
+      </div>
+      <div class="product_descripcion">
+          Sweater Winter
+      </div>
+      <div class="product_precio1">
+        $2890
+      </div>
+    </div>
+
+    <div class="product_container">
+      <div class="product_imagen">
+          <a href="index.html"><img src="img/camisabrickfuxia.jpg" alt="photo"></a>
+      </div>
+      <div class="product_descripcion">
+          Camisa Americana
+      </div>
+      <div class="product_precio1">
+        $2300
+      </div>
+     </div>
+
+     <div class="product_container">
+       <div class="product_imagen">
+           <a href="index.html"><img src="img/chaleco.jpg" alt="photo"></a>
+       </div>
+       <div class="product_descripcion">
+           Chaleco Alaska
+       </div>
+       <div class="product_precio1">
+         $2100
+       </div>
+     </div>
+  </div>
+  <!-- FIN DE PRODUCTOS MUESTRA -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,39 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
+});
+
+Route::get('/detalles', function () {
+    return view('detalles');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/carrito', function () {
+    return view('carrito');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/registro', function () {
+    return view('perfil');
+});
+
+Route::get('/login', function () {
+    return view('perfil');
 });
