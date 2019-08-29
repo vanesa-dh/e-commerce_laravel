560b05e6b3fb7b9201e4b3677d53b7d187aa4bb1
vanesa-dh <vanesap@gmx.com>
2019-08-29 17:05:45 -0300

    Solucionado registro de usuarios, agregado campo Usuario en Registro y su validacion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,6 +31,28 @@
                             </div>
                         </div>
 
+{{-- Inicio Agrego user_name --}}
+
+<div class="form-group row">
+    <label for="user_name" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+
+    <div class="col-md-6">
+        <input id="user_name" type="text" class="form-control @error('user_name') is-invalid @enderror" name="user_name" value="{{ old('user_name') }}" autocomplete="user_name" autofocus>
+
+        @error('user_name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+{{-- Fin Agrego user_name --}}
+
+
+
+
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
