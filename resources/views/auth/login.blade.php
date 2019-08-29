457614fa34936866a97fce9d0c29ab47a1d5a0f4
vanesa-dh <vanesap@gmx.com>
2019-08-28 22:07:14 -0300

    Estilo visual login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,24 @@
 @extends('plantilla')
+
 @section("titulo")
-  login - Vasanjohel
+  Login - Vasanjohel
 @endsection
+
 @section('principal')
+<div class="cart_container">
+
 <section class="div_container">
     <div class="login_container">
         <!-- <div class="h2_login"> -->
-             <div class="card">
-                <h2 class="h2_login">{{ __('Login') }}</h2>
+             <div>
+                {{-- <h4 class="">{{ __('Login') }}</h4> --}}
 
                 <!-- <div class="card-body"> -->
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="label_login">{{ __('E-Mail') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -55,12 +59,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button_register">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="hypervinc_login" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
@@ -72,4 +76,6 @@
         <!-- </div> -->
     </div>
 </section>
+
+</div>
 @endsection
