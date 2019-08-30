0c28d80656623ec7751696cbc906e8202f5d5380
vanesa-dh <vanesap@gmx.com>
2019-08-30 12:42:47 -0300

    Cambio minimo en Contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -14,7 +14,7 @@
   </div>
 
   <h3>Visitanos</h3>
-    <h4>Conocenos en nuestro SHOWROOM</h4>
+    <p>Conocenos en nuestro SHOWROOM!</p>
 
   <div class="iframe-container">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.979417243309!2d-58.38325006689879!3d-34.60469570214902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20CABA!5e0!3m2!1ses-419!2sar!4v1566699950431!5m2!1ses-419!2sar" frameborder="0"></iframe>
